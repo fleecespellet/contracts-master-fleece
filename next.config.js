33e46e1e143a9e7ff5094901119db6252baf07da
fleecespellet <fleeces_pellet0l@icloud.com>
2024-11-26 07:19:12 -0800

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/contracts-master-fleece',
+  images: {
+    unoptimized: true,
+  },
+}
+
+module.exports = nextConfig
